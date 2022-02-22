b6592afaaca47d1fe51f0b7aa0ca86f0ff75ddfc
andanylo <andanylo@gmail.com>
2022-02-22 16:59:04 -0500

    Start of new branch

---
+++ Assets/Scripts/ball.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+
+	private Sprite sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<Spirte>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
