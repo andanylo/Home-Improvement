b3e9933cb0c138da092349e73ca88bbf832fd2d4
andanylo <andanylo@gmail.com>
2022-03-15 16:55:07 -0400

    changes to movement system

--- Assets/Scripts/ButtonControl.cs
+++ Assets/Scripts/ButtonControl.cs
@@ -2,38 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using UnityStandardAssets.CrossPlatformInput;
 public class ButtonControl : MonoBehaviour
 {
 
     public Rigidbody2D rb;
 
-    private float movementSpeed = 5f;
+    private float movementSpeed = 2.5f;
+    private bool touchedDown = false;
+    public Button btn;
 
-    
-
-    public void RightDown(){
-        rb.velocity = Vector2.right * movementSpeed;
+    public void touchDownAction(){
+        this.touchedDown = true;
+    }
+    public void touchUpAction(){
+        this.touchedDown = false;
     }
 
-    public void TopDown(){
-        rb.velocity = Vector2.up * movementSpeed;
-    }
 
-    public void DownDown(){
-        rb.velocity = Vector2.down * movementSpeed;
-    }
-
-    public void LeftDown(){
-        rb.velocity = Vector2.left * movementSpeed;
-    }
 
     public void StopMoving(){
         rb.velocity = Vector2.zero;
     }
 
 
-    void FixedUpdate()
+
+    public void Update()
     {
-       // rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+        if (touchedDown == true){
+
+            Touch touch = Input.GetTouch(0);
+            
+            Vector2 offset = touch.position - new Vector2(btn.transform.position.x, btn.transform.position.y);
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+
+            rb.velocity = direction * movementSpeed;
+            Debug.Log(direction);
+        }
+        else{
+            rb.velocity = Vector2.zero;
+        }
     }
 }
